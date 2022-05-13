ceaca439c2e63f22a6b9747cb437ca3e816cc249
Awudi Eric Okyere <info@eoawudi.com>
2022-05-13 00:41:37 +0000

    server listing on port 3000 on localhost

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const morgan = require("morgan");
 const app = express();
 
 app.use(morgan("combined"));
-
+app.listen(3000);
 app.get("/", (req, res) => {
   res.send({ name: "hello there" });
 });
